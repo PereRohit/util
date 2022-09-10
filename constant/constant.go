2e88ac0641e02ac2c4a6c9adf410b2b74994121c
PereRohit <77770177+PereRohit@users.noreply.github.com>
2022-09-10 12:16:15 +0530

    Added missed constant package

---
+++ constant/constant.go
@@ -0,0 +1,14 @@
+package constant
+
+const (
+	SERVER_PORT_ENV     = "PORT"
+	DEFAULT_SERVER_PORT = "8080"
+)
+
+const(
+	UserAgentSvc = "html-to-pdf-service"
+)
+
+const(
+	HealthRoute = "/health"
+)
